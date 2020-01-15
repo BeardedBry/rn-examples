bc62497d035173e1a95be7d11857047445864d8b
Burkard <burkarbr@LAPTOP-BURKARBR.asddev.reyrey.com>
2020-01-15 17:11:34 -0500

    using dispatch

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -29,25 +29,25 @@ const SquareScreen = () => {
         <View>
             <Text>Square Screen</Text>
             <ColorCounter 
-                onIncrease={()=> dispatch(state, {colorToChange: 'red', COLOR_INCREMENT}) } 
-                // onDecrease={()=>} 
+                onIncrease={()=> dispatch({colorToChange: 'red', amount: COLOR_INCREMENT}) } 
+                onDecrease={()=> dispatch({colorToChange: 'red', amount: -1*COLOR_INCREMENT}) } 
                 color="Red"
             />
             <ColorCounter
-                // onIncrease={()=>} 
-                // onDecrease={()=>} 
+                onIncrease={()=> dispatch({colorToChange: 'blue', amount: COLOR_INCREMENT}) } 
+                onDecrease={()=> dispatch({colorToChange: 'blue', amount: -1*COLOR_INCREMENT}) } 
                 color="Blue" 
             />
             <ColorCounter
-                // onIncrease={()=>} 
-                // onDecrease={()=>} 
+                onIncrease={()=> dispatch({colorToChange: 'green', amount: COLOR_INCREMENT}) } 
+                onDecrease={()=> dispatch({colorToChange: 'green', amount: -1*COLOR_INCREMENT}) } 
                 color="Green"
             />
              <View 
                 style={{ 
                     height: 150, 
                     width: 150, 
-                    backgroundColor: `rgb(${red},${green},${blue})`
+                    backgroundColor: `rgb(${state.red},${state.green},${state.blue})`
                     }}
                 />
         </View>
